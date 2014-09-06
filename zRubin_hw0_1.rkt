58c3131a65f0ec08c2b204aac50fa5a25a6d6a09
rubinz <zevirc@gmail.com>
2014-09-06 14:52:18 -0400

    adding hw 2

--- zRubin_hw0_1.rkt
+++ zRubin_hw0_1.rkt
@@ -32,6 +32,9 @@
 
 )
 
-;(helperfunction (list 1 3 3) 7)
 
-(insertion-sort (list 1 3 2 6 3 2 6 0 0 7))
+(test (insertion-sort (list 1 2 3)) (list 1 2 3))
+(test (insertion-sort (list 3 2 1)) (list 1 2 3))
+(test (insertion-sort (list 1 3 1)) (list 1 1 3))
+(test (insertion-sort (list 1)) (list 1))
+(test (insertion-sort empty) empty)
